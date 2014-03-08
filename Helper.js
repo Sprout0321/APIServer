24fb61fc44c1af0524ef25ea1f4c6174b3ea7b91
Muqq <bbbb55952000@gmail.com>
2014-03-09 04:46:27 +0800

    Helper.js

---
+++ Helper.js
@@ -0,0 +1,41 @@
+var _ = require('lodash');
+exports.ConverRecord = function(item){
+  var result = {} ;
+  for (var k in item){
+    if(item[k].N){
+      result[k] = parseInt(item[k].N);
+    }
+    else if (item[k].S){
+      result[k] = item[k].S ;
+    }
+    else if (item[k].NS){
+      result[k] = [];
+      item[k].NS.forEach(function(n){
+	result[k].push(parseInt(n));
+      });
+    }
+    else if (item[k].SS){
+      result[k] = item[k].SS ;
+    }
+    else{
+      throw new Error('Cannot conver record with B/BS type');
+    }
+  }
+  return result;
+}
+exports.CheckNumber = function(item){
+  var re = /^[0-9]+$/;
+  if(!re.test(item.toString())){
+    return false ;
+  }else{
+    item = parseInt(item);
+  }
+  if(_.isNumber(item)){
+    item = item.toString();
+  if((item.indexOf('.') != -1))
+    return false;
+  }else{
+    return false ;
+  }
+  return true; 
+}
