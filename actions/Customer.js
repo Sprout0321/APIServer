c6f757ea956175a37fbd5229bab1aa7589e6586f
root <root@ip-172-31-29-54.ap-northeast-1.compute.internal>
2014-03-09 04:41:28 +0800

    get

--- actions/Customer.js
+++ actions/Customer.js
@@ -22,3 +22,28 @@ exports.CreateCustomer = {
 		});
 	}
 }
+
+exports.GetCustomer = {
+	name: 'GetCustomer',
+	description: 'Get customer in customer table using in client app',
+	version:1.0,
+	inputs:{
+		required: ['email','password'],
+		optional:[],
+	},
+	run: function(api, connection, next){
+		var params = {
+			email : connection.params.email,
+			password: connection.params.password
+		}
+		api.service.customer.Get(params, function(error, result){
+			if(error){
+				connection.error = error ;
+				return next(connection, true);
+			}else{
+				connection.response = result;
+				return next(connection, true);
+			}
+		});
+	}
+}
